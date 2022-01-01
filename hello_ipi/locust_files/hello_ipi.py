035f21878d5d69579443865158fc9147e9e143b3
Joan Killeen <joan.killeen@bluewin.ch>
2022-01-01 15:01:37 +0100

    Search for links on page and randomly select

--- hello_ipi/locust_files/hello_ipi.py
+++ hello_ipi/locust_files/hello_ipi.py
@@ -1,10 +1,47 @@
-from locust import HttpUser, task
+import random
+import logging
+import sys
+from locust import between, task, HttpUser
+from pyquery import PyQuery
 
 class HelloIPI(HttpUser):
+
+    # we assume someone who is browsing the page, 
+    # generally has a quite long waiting time (between 
+    # 10 and 600 seconds), since there's a bunch of text 
+    # on each page
+    wait_time = between(10, 600)
+    
+
+    def on_start(self):
+        # Set root logging
+        logging.getLogger().setLevel('INFO')
+
+        # Add stdout to locust logging
+        logger = logging.getLogger('locust')
+        logger.addHandler(logging.StreamHandler(sys.stdout))
+        
+        
+        self.hello_ipi()
+        self.urls_on_current_page = self.urls_found
+        #logging.info("Urls found: " + str(len(self.urls_on_current_page)))
+        
+    
+    
     @task
     def hello_ipi(self):
-        self.client.get("/etwas-schuetzen")
+        request = self.client.get("")
+        my_query = PyQuery(request.content)
 
-    @task
-    def test_fail_url(self):
-        self.client.get("/nonsense")
+        # Find other links on the page
+        link_elements = my_query(".ige_teasertopimage_teaser a")
+        self.urls_found = [
+            l.attrib["href"] for l in link_elements
+        ]
+
+    @task(5)
+    def load_page(self):
+        url = random.choice(self.urls_on_current_page)
+        r = self.client.get(url)
+
+    
