f16efa0147c23b159e2d8f6e468837ebf06026e4
Joan Killeen <joan.killeen@bluewin.ch>
2022-01-02 15:56:01 +0100

    Changed structure

--- hello_ipi/locust_files/hello_ipi.py
+++ hello_ipi/locust_files/hello_ipi.py
@@ -5,30 +5,17 @@ from locust import between, task, HttpUser
 from pyquery import PyQuery
 
 class HelloIPI(HttpUser):
-
-    # we assume someone who is browsing the page, 
-    # generally has a quite long waiting time (between 
-    # 10 and 600 seconds), since there's a bunch of text 
-    # on each page
+    
     wait_time = between(10, 600)
     
 
-    def on_start(self):
-        # Set root logging
-        logging.getLogger().setLevel('INFO')
-
-        # Add stdout to locust logging
-        logger = logging.getLogger('locust')
-        logger.addHandler(logging.StreamHandler(sys.stdout))
-        
-        
+    def on_start(self):      
         self.hello_ipi()
         self.urls_on_current_page = self.urls_found
-        #logging.info("Urls found: " + str(len(self.urls_on_current_page)))
+        print("Urls found: " + str(len(self.urls_on_current_page)))
         
     
-    
-    @task
+
     def hello_ipi(self):
         request = self.client.get("")
         my_query = PyQuery(request.content)
@@ -39,7 +26,7 @@ class HelloIPI(HttpUser):
             l.attrib["href"] for l in link_elements
         ]
 
-    @task(5)
+    @task
     def load_page(self):
         url = random.choice(self.urls_on_current_page)
         r = self.client.get(url)
